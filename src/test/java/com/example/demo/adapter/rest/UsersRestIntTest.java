5f35bf6f8564eb5d62a6363a21a5cb7de460593e
maliaspire <42336166+maliaspire@users.noreply.github.com>
2018-09-02 18:26:33 +0300

    fix the project

--- src/test/java/com/example/demo/adapter/rest/UsersRestIntTest.java
+++ src/test/java/com/example/demo/adapter/rest/UsersRestIntTest.java
@@ -16,7 +16,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.junit.runners.Parameterized.*;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
